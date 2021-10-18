b128eb1e82af0d807673943b53514b4db588455c
Alex <secondsmall86@gmail.com>
2021-10-18 11:48:05 -0400

    Update Microsoft.PowerShell_profile.ps1

--- devTools/PowershellConfig/Microsoft.PowerShell_profile.ps1
+++ devTools/PowershellConfig/Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,18 @@
 Import-Module posh-git
 Import-Module oh-my-posh
 
-function gs {git status}
+function docbash {docker exec -it $args bash}
+function docs {docker start $args}
+function gitb {git branch}
+function gitc {git checkout $args}
+function gits {git status}
 function ii {Invoke-Item}
-function gb {git branch}
-function gcheckout {git checkout $args}
+
 
 Set-Theme PowerLine
 
-Set-Alias sublime 'C:\GeneralApp\Sublime\Sublime Text Build 3211\sublime_text.exe'
-Set-Alias code 'C:\DevRelated\VSCode-win32-x64-1.54.2\bin\code.cmd'
+Set-Alias sublime 'C:\Development\Sublime Text Build 3211\sublime_text.exe'
+Set-Alias code 'C:\Edge\development\VSCode-win32-x64-1.52.1\bin\code.cmd'
 Set-Alias gcheck gcheckout
 
 Set-Theme PowerLine
