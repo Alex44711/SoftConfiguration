9eeb79a4ee82573c30a226eda817b3a0af2c2de4
Alex <secondsmall86@gmail.com>
2022-10-18 10:44:38 -0400

    Update Microsoft.PowerShell_profile.ps1

--- devTools/PowershellConfig/Microsoft.PowerShell_profile.ps1
+++ devTools/PowershellConfig/Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,25 @@
 function docbash {docker exec -it $args bash}
 function docs {docker start $args}
-function gitb {git branch}
+function gitb {git branch $args}
 function gitc {git checkout $args}
+function gitD {git branch -D $args}
 function gits {git status}
 function gitpr {git pull --rebase}
+function gitp {
+  $env:current_branch=$(git branch --show-current)
+  git push origin $env:current_branch:$env:current_branch
+}
 function ii {Invoke-Item}
 function kc {kubectxwin $args}
 function kn {kubenswin $args}
+function k {kubectl $args}
 
-Set-Alias sublime 'C:\Development\Sublime Text Build 3211\sublime_text.exe'
-Set-Alias code 'C:\Edge\development\VSCode-win32-x64-1.52.1\bin\code.cmd'
+Set-Alias sublime '<path>\sublime_text.exe'
+Set-Alias code '<path>\code.cmd'
+Set-Alias k kubectl
+Set-Alias kn kubenswin
+Set-Alias kc kubectxwin
+
 Set-Alias gcheck gcheckout
 
 remove-item alias:curl
