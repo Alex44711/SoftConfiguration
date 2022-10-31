fe12f3c8708e8ef8dae53d831e336ae7b74ad7cb
Alex <secondsmall86@gmail.com>
2022-10-31 11:37:46 -0400

    Update bashConfig.sh

--- devTools/bash/bashConfig.sh
+++ devTools/bash/bashConfig.sh
@@ -8,13 +8,18 @@ alias gits='git status'
 alias gitb='git branch'
 alias gitpr='git pull --rebase'
 gitc() {
-  git checkout "$1"
+  git checkout "$@"
 }
-gitpo() {
+gitd() {
+  git branch -D "$@"
+}
+gitp() {
   export CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
   git push origin $CURRENT_BRANCH:$CURRENT_BRANCH
 }
 
 # Kubernetes Alias
 alias k='kubectl'
+alias kc='kubectx'
+alias kn='kubens'
 source <(kubectl completion bash | sed s/kubectl/k/g)
